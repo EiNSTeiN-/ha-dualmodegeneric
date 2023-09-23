9bc6a291dc54bab76ffbcf2ce1ddea7ddb1d9d46
jack3308 <69616831+jack3308@users.noreply.github.com>
2023-09-23 22:26:54 +1000

    Update __init__.py

--- custom_components/dualmode_generic/__init__.py
+++ custom_components/dualmode_generic/__init__.py
@@ -2,3 +2,8 @@
 
 DOMAIN = "proxy_climate"
 PLATFORMS = ["climate"]
+
+async def async_setup(hass, config):
+    """Set up the dualmode_ac climate component."""
+    # No additional setup is required in this file
+    return True
